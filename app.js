297483a9d03a04f256263e3f5a721d765b5766de
Dharmik Ahir <sangadharmik78@gmail.com>
2026-04-30 14:44:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+
+import express from "express";
+
+
+const app = express();
+
+app.get ("/", (req , res)=> {
+    res.send(" This is home page :");
+
+});
+const port = 5000; 
+
+app.listen(port,(err)=>{
+
+    console.log(`This  code is lodded.. ${port}`);
+
+});
